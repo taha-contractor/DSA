9f4b2e1e264e8b860026553d484776c063c86e77
Taha Contractor <tasaif265@gmail.com>
2026-09-17 17:24:17 +0530

    Problem 121 : Best Time to Buy and Sell Stocks

--- Arrays/StockExchange.java
+++ Arrays/StockExchange.java
@@ -1,31 +1,23 @@
-/*Problem: Best Time to Buy and Sell Stock
+/*
+Leetcode Problem: 121. Best Time to Buy and Sell Stock
 
-Given:
+You are given an array prices where prices[i] is the price of a given stock on the ith day. You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock. Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
-[7, 1, 5, 3, 6, 4]
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
-Each number represents the stock price on that day.
-
-You can:
-
-Buy once
-Sell once
-You must buy before selling.
-
-Find the maximum profit.
-
-Expected:
-
-Buy at 1
-Sell at 6
-
-
-Profit = 6 - 1 = 5
-More examples
-[7, 6, 4, 3, 1]
-→ 0
-
-Because there's no profitable transaction.*/
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+Constraints:
+1 <= prices.length <= 10^5
+0 <= prices[i] <= 10^4
+*/
 
 public class StockExchange {
     public static int maxProfit(int[] prices){
