31aead163fcf7107ce8b1b3910167ff914aba037
Taha Contractor <tasaif265@gmail.com>
2026-09-17 19:28:13 +0530

    Problem 169 : Majority Element

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,39 @@
+/* 
+Leetcode Problem 169: Majority Element
+
+Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+Constraints:
+n == nums.length
+1 <= n <= 5 * 10^4
+-10^9 <= nums[i] <= 10^9
+The input is generated such that a majority element will exist in the array.
+*/
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (candidate == nums[i]) {
+                count++;
+            }
+            count--;
+        }
+        return candidate;
+    }
+    public static void main(String[] args) {
+        MajorityElement me = new MajorityElement();
+        int[] nums = {3, 2, 3};
+        System.out.println(me.majorityElement(nums));
+    }
+    
+}
