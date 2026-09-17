e2c6f992b4252bb4d7132075ac631a60c69322aa
Taha Contractor <tasaif265@gmail.com>
2026-09-17 16:10:13 +0530

    Problem 01 : Two Sum

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -20,24 +20,28 @@ Constraints:
 -10*9 <= target <= 10*9
 Only one valid answer exists. */
 
-class TwoSum {
-    public static void main(String[] args) {
-        int[] nums = {15, 15};
-        int target = 30;
-        int[] result = twoSum(nums, target);
-        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
-    }
-    public static int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        int[] result = new int[2];
-        for (int i = 0; i < n; i++) {
-            for (int j =0; j < n; j++) {
-                if (nums[i] + nums[j] == target && i != j) {
-                    result[0] = j;
-                    result[1] = i;
-                }
+import java.util.HashMap;
+import java.util.Map;
+public class TwoSum {
+    public static int[  ] findSum(int[] arr, int target){
+        Map<Integer, Integer>seen = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int comp = target - arr[i];
+            if (seen.containsKey(comp)) {
+                return new int[]{seen.get(comp), i};
             }
+            seen.put(arr[i], i);
         }
-        return result;
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = findSum(arr, target);
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two numbers found that add up to the target.");
+        }
     }
 }
