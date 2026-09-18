a21ea3707ad8938d410e91c4ca9b8d5bee88bd58
Taha Contractor <tasaif265@gmail.com>
2026-09-18 22:16:43 +0530

    Problem 189 : Rotate Array

---
+++ Arrays/RotateArray.java
@@ -0,0 +1,53 @@
+/*
+Leetcode Problem: 189. Rotate Array
+
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+
+Constraints:
+1 <= nums.length <= 10^5
+-2^31 <= nums[i] <= 2^31 - 1
+0 <= k <= 10^5
+*/
+
+class RotateArray {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    
+    public static void main(String[] args) {
+        RotateArray ra = new RotateArray();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        ra.rotate(nums, k);
+        System.out.println(Arrays.toString(nums));
+    }
+}
